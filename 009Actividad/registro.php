2edc110c6330f670316b7fd686feac09b278e6c5
GMartinMx78UTEQ <gabriel.martin@uteq.edu.mx>
2021-06-02 13:30:06 -0500

    t

--- 009Actividad/registro.php
+++ 009Actividad/registro.php
@@ -1,2 +1,35 @@
 <html>
+    <head>
+        <title>Registro</title>
+    </head>
+    <body>
+        <form action="validalogin.php" method="POST">
+            <table style="width:50%;">
+                <tr>
+                    <td>Usuario:</td>
+                    <td><input type="text" name="usralias" id="usralias" /></td>
+                </tr>
+                <tr>
+                    <td>
+                        Escriba una nueva contraseña:
+                    </td>
+                    <td>
+                        <input type="password" name="passuno" />
+                    </td>
+               </tr>
+               <tr>
+                    <td>
+                        Repita la nueva contraseña:
+                    </td>
+                    <td>
+                        <input type="password" name="passdos" />
+                    </td>
+               </tr>
+                <tr>
+                    <td colspan="2"><input type="submit" value="Crear usuario"/></td>
+                </tr>
+               
+            </table>
+        </form>
+    </body>
 </html>
