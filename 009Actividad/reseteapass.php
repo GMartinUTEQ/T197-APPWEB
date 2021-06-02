8bdf2794c2aa5c6c8de65ae63a5d42bf51917223
GMartinMx78UTEQ <gabriel.martin@uteq.edu.mx>
2021-06-02 12:52:56 -0500

    y

---
+++ 009Actividad/reseteapass.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>Resetea tu contraseña</title>
+    </head>
+    <body>
+        <form>
+            <table style="width:50%">
+                <tr>
+                    <td>
+                        Escriba una nueva contraseña:
+                    </td>
+                    <td>
+                        <input type="password" name="passuno" />
+                    </td>
+               </tr>
+               <tr>
+                    <td>
+                        Repita la nueva contraseña:
+                    </td>
+                    <td>
+                        <input type="password" name="passdos" />
+                    </td>
+               </tr>
+               <tr>
+                    <td colspan="2">
+                    </td>
+               </tr>
+            </table>
+        </form>
+    </body>
+</html>
