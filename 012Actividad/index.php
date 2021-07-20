a0a7ef23c20c79d57ac834584042b7b94457a1ea
GMartin <gabosoft@gmail.com>
2021-07-20 07:33:11 -0500

    12Activ

---
+++ 012Actividad/index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Catálogo de productos</title>
+    </head>
+    <body>
+        <form>
+            ID producto:<br/>
+            <input type="text" id="idpro" nombre="idpro"/><br/>
+            Nombre producto:<br/>
+            <input type="text" id="nompro" nombre="nompro"/><br/>
+            Precio producto:<br/>
+            <input type="text" id="idpro" nombre="idpro"/><br/>
+            Talla:<br/>
+            <select id="talla" name="talla">
+                <option value="Ch">Chica</option>
+                <option value="Md">Mediana</option>
+            </select><br/>
+            Marca:<br/>
+            <select id="marca" name="marca">
+                <option value="AE">American Eagle</option>
+                <option value="GAP">GAP</option>
+            </select><br/>
+            <input type="submit" value="Guardar"/>
+
+        </form>
+    </body>
+</html>
