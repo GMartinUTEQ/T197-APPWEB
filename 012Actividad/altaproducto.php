9bfa091da2be6031c0377954157080306398d10a
GMartin <gabosoft@gmail.com>
2021-07-20 08:20:27 -0500

    selectfrommysql

--- 012Actividad/altaproducto.php
+++ 012Actividad/altaproducto.php
@@ -7,7 +7,7 @@ if ($conn->connect_error) {
 }
 
 $sql = "INSERT INTO producto (nombre, precio, idmarca, idtalla) VALUES ('".$_REQUEST["nompro"]."', ". $_REQUEST["preciopro"] .", '". $_REQUEST["marca"] ."', '" . $_REQUEST["talla"] . "')";
- var_dump($_REQUEST);
+ //var_dump($_REQUEST);
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
 } else {
--- 012Actividad/index.php
+++ 012Actividad/index.php
@@ -12,13 +12,51 @@
             <input type="text" id="preciopro" name="preciopro"/><br/>
             Talla:<br/>
             <select id="talla" name="talla">
-                <option value="1">Chica</option>
-                <option value="2">Mediana</option>
+                <option value="0">==Seleccione una talla==</option>
+                <?php
+                    include("conexion.php");
+                    // Check connection
+                    if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                    }
+
+                    $sql = "Select * from talla";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        echo "<option value='" . $row["idtalla"] . "'>" . $row["talla"] . "</option>";
+                    }
+                    } else {
+                        echo "<option>Ninguno</option>";
+                    }
+                    $conn->close();
+                ?>
             </select><br/>
             Marca:<br/>
             <select id="marca" name="marca">
-                <option value="1">American Eagle</option>
-                <option value="2">GAP</option>
+            <option value="0">==Seleccione una marca==</option>
+                <?php
+                    include("conexion.php");
+                    // Check connection
+                    if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                    }
+
+                    $sql = "Select * from marca";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        echo "<option value='" . $row["idmarca"] . "'>" . $row["marca"] . "</option>";
+                    }
+                    } else {
+                        echo "<option>Ninguno</option>";
+                    }
+                    $conn->close();
+                ?>  
             </select><br/>
             <input type="submit" value="Guardar"/>
 
