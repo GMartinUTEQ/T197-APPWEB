a008330eecf3c6d7b369f46a3a6de53666f62458
GMartin <gabosoft@gmail.com>
2021-07-23 09:01:13 -0500

    graficas

--- 012Actividad/eliminaimagen.php
+++ 012Actividad/eliminaimagen.php
@@ -37,7 +37,7 @@ else
 $sql = "delete from imagen where idimagen =  " . $_REQUEST["idimg"];
  //var_dump($_REQUEST);
 if ($conn->query($sql) === TRUE) {
-    echo "<script>window.location.href='listaprod.php'</script>";
+    eecho "<script>window.location.href='listaprod.php'</script>";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
---
+++ 013Actividad/index.php
@@ -0,0 +1,63 @@
+<html>
+    <head>
+        <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+   
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+    </head>
+
+    <body class="align-items-center">
+
+    <div class="card " style="width:80%;margin-top:40px;margin-left:40px">
+        <div class="card-header">
+            Visitantes
+        </div>
+        <div class="card-body">
+           
+        
+
+        <div id="chart_bar" style="height: 250px;"></div>
+        <?php
+        include("conexion.php");
+        // Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "select
+        (select count(registrado) as nr from visitas where registrado = 0) as noregistrados,
+        (select count(registrado) as r from visitas where registrado = 1) as registrados;
+        ";
+        
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<div id='chart_bar'></div>
+                    <script>
+                    Morris.Bar({
+                        element: 'chart_bar',
+                        data: [
+                        { visitante: 'Registrados', nb: " . $row["registrados"] . " },
+                        { visitante: 'No Registrados', nb: " . $row["noregistrados"] . " }
+                        ],
+                        xkey: 'visitante',
+                        ykeys: ['nb'],
+                        labels: ['Visitantes']
+                    });
+            </script>";
+        }
+        } else {
+        echo "0 results";
+        }
+        $conn->close();
+        ?>
+        </div>
+        </div>
+    </body>
+</html>
+
